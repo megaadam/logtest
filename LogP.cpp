57aa2c761bba18bb7965e7a7b9ae57e2b940815d
megaadam <adam.horvath@edgeware.tv>
2017-07-12 12:40:38 +0200

    Make proper temp object and add optional log data

--- LogP.cpp
+++ LogP.cpp
@@ -2,3 +2,8 @@
 
 LOG::logLevel LogProxy::currentLevel = LOG::ERROR;
 
+bool LogProxy::logMillisec = true;
+bool LogProxy::logThreadId = true;
+bool LogProxy::logFilename = false;
+bool LogProxy::logLinenum = false;
+bool LogProxy::logPrettyFunc = true;
--- LogP.h
+++ LogP.h
@@ -1,8 +1,16 @@
 #pragma once
 
+#include <pthread.h>
+
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <iostream>
 #include <string>
 #include <sstream>
-#include <iostream>
+#include <thread>
+
+
 namespace LOG {
 	enum logLevel
 	{
@@ -18,44 +26,139 @@ namespace LOG {
 			using namespace LOG; \
 			if(messageLevel >= LogProxy::getCurrentLevel()) \
 			{ \
-				LogProxy logProxy; \
-				logProxy << " | "  << " => " << __PRETTY_FUNCTION__ << ":" << __LINE__ << " " << s << std::endl; \
+				LogProxy(__FILE__, __LINE__, __PRETTY_FUNCTION__) << " | "  << s; \
 			} \
 		}
 
 class LogProxy
 {
 public:
-	LogProxy()
+	LogProxy(const std::string& fileName, const unsigned int lineNum, const std::string& prettyFunc)
 	{
 
+		auto now = std::chrono::system_clock::now();
+		auto now_c = std::chrono::system_clock::to_time_t(now);
+		mStringStream << std::put_time(std::localtime(&now_c), "%F %T");
+
+		if(logMillisec == true)
+		{
+			auto milli = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+			mStringStream << "." << (milli % 1000);
+		}
+
+		if(logThreadId == true)
+		{
+			mStringStream << " | " << pthread_self();
+		}
+
+		if(logFilename == true)
+		{
+			mStringStream << " | " << fileName;
+		}
+
+		if(logLinenum == true)
+		{
+			mStringStream << " :" << lineNum;
+		}
+
+		if(logPrettyFunc == true)
+		{
+			mStringStream << " | " << prettyFunc;
+		}
 	}
 
 	~LogProxy()
 	{
-        std::cout << mStringStream.str().c_str();
+        std::cout << mStringStream.str().c_str() << std::endl << std::endl << std::endl;
 	}
 
 	std::stringstream& operator<<(const std::string& ss)
 	{
-		mStringStream.clear();
-		mStringStream.str(std::string());
-		mStringStream << ss;
+		mStringStream << ss ;
 		return mStringStream;
 	}
 
-	static LOG::logLevel getCurrentLevel()
+
+	// currentLevel
+	static inline LOG::logLevel getCurrentLevel()
 	{
 		return currentLevel;
 	}
 
-	static void setCurrentLevel(LOG::logLevel l)
+	static inline void setCurrentLevel(LOG::logLevel l)
 	{
 		currentLevel = l;
 	}
 
+
+	// logMillisec
+	static inline bool getLogMillisec()
+	{
+		return logMillisec;
+	}
+
+	static inline void setLogMillisec(bool l)
+	{
+		logMillisec = l;
+	}
+
+
+	// logThreadId
+	static inline bool getlogThreadId()
+	{
+		return logThreadId;
+	}
+
+	static inline void setlogThreadId(bool l)
+	{
+		logThreadId = l;
+	}
+
+
+	// logFilename
+	static inline bool getlogFilename()
+	{
+		return logFilename;
+	}
+
+	static inline void setlogFilename(bool l)
+	{
+		logFilename = l;
+	}
+
+
+	// logLinenum
+	static inline bool getlogLinenum()
+	{
+		return logLinenum;
+	}
+
+	static inline void setlogLinenum(bool l)
+	{
+		logLinenum = l;
+	}
+
+
+	// logPrettyFunc
+	static inline bool getlogPrettyFunc()
+	{
+		return logPrettyFunc;
+	}
+
+	static inline void setlogPrettyFunc(bool l)
+	{
+		logPrettyFunc = l;
+	}
+
+
 private:
 	std::stringstream mStringStream;
 	static LOG::logLevel currentLevel;
+
+	static bool logMillisec;
+	static bool logThreadId;
+	static bool logFilename;
+	static bool logLinenum;
+	static bool logPrettyFunc;
 };
 
